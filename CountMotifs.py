4bc6f454ed9c728a99df6271ab7d02a7af06255a
Satavisha Roy <satavisha.roy@gmail.com>
2018-06-24 23:41:17 -0400

    Create CountMotifs.py

---
+++ CountMotifs.py
@@ -0,0 +1,19 @@
+# Input:  A set of kmers Motifs
+# Output: Count(Motifs)
+def Count(Motifs):
+    count = {} # initializing the count dictionary
+    # your code here
+    k = len(Motifs)
+    for symbol in "ACTG":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(0)
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    return count                                                                                                                                                                                                                            
+### DO NOT MODIFY THE CODE BELOW THIS LINE ###
+import sys
+print(Count(sys.stdin.read().splitlines()))
