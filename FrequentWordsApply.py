07301c49aeecc7ce51743c7d695046bac47b405e
Satavisha Roy <satavisha.roy@gmail.com>
2018-06-24 23:27:34 -0400

    Create FrequentWordsApply.py

---
+++ FrequentWordsApply.py
@@ -0,0 +1,49 @@
+# Copy your updated FrequentWords function (along with all required subroutines) below this line
+# Input:  A string Text and an integer k
+# Output: A list containing all most frequent k-mers in Text
+def FrequentWords(Text, k):
+    FrequentPatterns = [] # output variable
+    Count = CountDict(Text, k)
+    m = max(Count.values())
+    for i in Count:
+        if Count[i] == m:
+            FrequentPatterns.append(Text[i:i+k])
+    FrequentPatternsNoDuplicates = remove_duplicates(FrequentPatterns)
+    return FrequentPatternsNoDuplicates
+
+# Input:  A string Text and an integer k
+# Output: CountDict(Text, k)
+def CountDict(Text, k):
+    Count = {} # output variable
+    for i in range(len(Text)-k+1):
+        Pattern = Text[i:i+k]
+        Count[i] = PatternCount(Pattern, Text)
+    return Count
+
+# Input:  Strings Pattern and Text
+# Output: The number of times Pattern appears in Text
+def PatternCount(Pattern, Text):
+    count = 0 # output variable
+    for i in range(len(Text)-len(Pattern)+1):
+        if Text[i:i+len(Pattern)] == Pattern:
+            count = count+1
+    return count
+
+def remove_duplicates(Items):
+    ItemsNoDuplicates = [] # output variable
+    for i in Items:
+        if i not in ItemsNoDuplicates:
+            ItemsNoDuplicates.append(i)
+    return ItemsNoDuplicates
+
+# Now set Text equal to the Vibrio cholerae oriC and k equal to 10
+Text = "ATCAATGATCAACGTAAGCTTCTAAGCATGATCAAGGTGCTCACACAGTTTATCCACAACCTGAGTGGATGACATCAAGATAGGTCGTTGTATCTCCTTCCTCTCGTACTCTCATGACCACGGAAAGATGATCAAGAGAGGATGATTTCTTGGCCATATCGCAATGAATACTTGTGACTTGTGCTTCCAATTGACATCTTCAGCGCCATATTGCGCTGGCCAAGGTGACGGAGCGGGATTACGAAAGCATGATCATGGCTGTTGTTCTGTTTATCTTGTTTTGACTGAGACTTGTTAGGATAGACGGTTTTTCATCACTGACTAGCCAAAGCCTTACTCTGCCTGACATCGACCGTAAATTGATAATGAATTTACATGCTTCCGCGACGATTTACCTCTTGATCATCGATCCGATTGAAGATCTTCAATTGTTAATTCTCTTGCCTCGACTCATAGCCATGATGAGCTCTTGATCATGTTTCCTTAACCCTCTATTTTTTACGGAAGAATGATCAAGCTGCTGCTCTTGATCATCGTTTC"
+k = 10
+
+# Finally, print the result of calling FrequentWords on Text and k.
+FrequentWords(Text, k)
+import sys
+lines = sys.stdin.read().splitlines()
+#print(' '.join(FrequentWords(lines[0],int(lines[1]))))
+answer = FrequentWords(lines[0],int(lines[1]))
+print(answer)
